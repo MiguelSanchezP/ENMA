83dd53c8cad485f1be60267d084fb64265b5b420
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-08-12 23:01:40 +0200

    deleted the erroneous algorithm for the transposition cypher

--- src/miguelsanchezp/ENMA/methods/Transposition.java
+++ src/miguelsanchezp/ENMA/methods/Transposition.java
@@ -2,21 +2,6 @@ package miguelsanchezp.ENMA.methods;
 
 public class Transposition {
     public static String cypher (String message, String key) {
-        StringBuilder sb = new StringBuilder();
-        int position = 0;
-        int lap = 0;
-        for (int i = 0; i<message.length(); i++) {
-            if (position == key.length()) {
-                position=0;
-                lap += 1;
-            }
-            int val = position+48;
-            char num = (char)val;
-            System.out.println(num);
-            System.out.println(message.charAt(key.length()*lap+key.indexOf(num)));
-            sb.append(message.charAt(key.length()*lap+key.indexOf(num)));
-            position++;
-        }
-        return sb.toString();
+
     }
 }
