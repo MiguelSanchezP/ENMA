3e26e6f9b8394cb6472df7580a7e42f9a2718b51
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-08-13 10:21:57 +0200

    fixed the transposition cyphering algorithm

--- src/miguelsanchezp/ENMA/methods/Transposition.java
+++ src/miguelsanchezp/ENMA/methods/Transposition.java
@@ -2,6 +2,20 @@ package miguelsanchezp.ENMA.methods;
 
 public class Transposition {
     public static String cypher (String message, String key) {
-
+        int pos = 0;
+        StringBuilder cyphered = new StringBuilder();
+        int vals = message.length();
+        while (vals>0) {
+            int cypherChar = ((pos/key.length())*key.length());
+            int character = Character.getNumericValue(key.charAt(pos%key.length()));
+            if (!((cypherChar+character) >= message.length())) {
+                cyphered.append(message.charAt(cypherChar+character));
+                vals--;
+            }else{
+                cyphered.append('X');
+            }
+            pos++;
+        }
+        return cyphered.toString();
     }
 }
