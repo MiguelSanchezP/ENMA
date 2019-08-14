5c3a9a4f400d7d351b2358e1c237daa5a095d52c
miguelsanchezp <miguelsanchez1703@gmail.com>
2019-08-14 10:19:43 +0200

    fixed the vertical algorithm + return in upper case

--- src/miguelsanchezp/ENMA/methods/Transposition.java
+++ src/miguelsanchezp/ENMA/methods/Transposition.java
@@ -1,5 +1,7 @@
 package miguelsanchezp.ENMA.methods;
 
+import miguelsanchezp.ENMA.resources.Utilities;
+
 public class Transposition {
     public static String cypherHorizontal (String message, String key) {
         int pos = 0;
@@ -16,13 +18,11 @@ public class Transposition {
             }
             pos++;
         }
-        return cyphered.toString();
+        return cyphered.toString().toUpperCase();
     }
 
     public static String cypherVertical (String message, String key) {
-//        int pos = 0;
         StringBuilder cyphered = new StringBuilder();
-//        int vals = message.length();
         int rounds = message.length()%key.length();
         if (message.length()%key.length()!=0) {
             rounds=message.length()%key.length()+1;
@@ -32,9 +32,13 @@ public class Transposition {
             for (int j = 0; j<rounds; j++) {
                 if (j*key.length()+keyVal < message.length()) {
                     cyphered.append(message.charAt(j*key.length()+keyVal));
+                }else{
+                    if (j*key.length()+keyVal < Utilities.nearestUpperMultiple(message.length(), key.length())) {
+                        cyphered.append('X');
+                    }
                 }
             }
         }
-        return cyphered.toString();
+        return cyphered.toString().toUpperCase();
     }
 }
